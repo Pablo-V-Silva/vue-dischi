2d9bdabbed88d9b83c0410758575c62b2d882467
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-15 12:33:58 +0100

    add default card before create component for single card

--- src/components/sitemain.vue
+++ src/components/sitemain.vue
@@ -1,10 +1,26 @@
 <template>
-  <main></main>
+  <main>
+    <div class="card card_color" style="width: 18rem">
+      <img src="" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title text-light text-center">Card title</h5>
+        <p class="card-text text-light text-center">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+
+.card_color {
+  background-color: $card_color;
+}
 </style>
