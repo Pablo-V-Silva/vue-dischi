b0d71029a70bd39eb9d7372fbb1c60961035a11b
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-15 12:24:12 +0100

    header complete

--- src/components/siteheader.vue
+++ src/components/siteheader.vue
@@ -1,10 +1,19 @@
 <template>
-  <header></header>
+  <div class="header">
+    <div class="contain">
+      <img class="my-4" src="../assets/img/logo.svg" alt="" height="80px" />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+
+.header {
+  background-color: $card_color;
+}
 </style>
