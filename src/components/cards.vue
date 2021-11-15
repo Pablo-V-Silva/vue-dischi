45a5d35c7532073c35677ab8037c5886dce08966
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-15 15:47:38 +0100

    v-for complete in success

---
+++ src/components/cards.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="row row-cols-2 row-cols-sm-4 row-cols-lg-6">
+    <div class="col" v-for="card in cards" :key="card.title">
+      <div class="card_color" style="width: 18rem">
+        <img
+          :src="card.poster"
+          class="card-img-top"
+          :alt="card.title"
+          width="90%"
+        />
+        <div class="card-body">
+          <h5 class="card-title text-light text-center">{{ card.title }}</h5>
+          <p class="card-text text-light text-center">{{ card.author }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      cards: [],
+    };
+  },
+  components: {},
+
+  mounted() {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/array/music")
+      .then((resp) => {
+        this.cards = resp.data.response;
+        console.log(resp.data.response);
+      });
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../assets/scss/variables.scss";
+
+.card_color {
+  background-color: $card_color;
+}
+</style>
--- src/components/sitemain.vue
+++ src/components/sitemain.vue
@@ -1,26 +1,20 @@
 <template>
   <main>
-    <div class="card card_color" style="width: 18rem">
-      <img src="" class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title text-light text-center">Card title</h5>
-        <p class="card-text text-light text-center">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-      </div>
+    <div class="container-fluid">
+      <Card />
     </div>
   </main>
 </template>
 
 <script>
-export default {};
+import Card from "./cards.vue";
+
+export default {
+  components: {
+    Card,
+  },
+};
 </script>
 
 <style lang="scss">
-@import "../assets/scss/variables.scss";
-
-.card_color {
-  background-color: $card_color;
-}
 </style>
